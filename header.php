8410455106b4089c9cae603232d2ecae04932616
Amadou camara <atidiane.camara@gmail.com>
2017-10-04 12:07:56 +0200

    ajout de tout le head à header.php

--- header.php
+++ header.php
@@ -1,13 +1,24 @@
 
-<div class="sign">
-  <a href="#">Inscription</a>
-  <a href="#">Connexion</a>
-</div>
-<nav class="navbar navbar-default navbar-static-top">
-  <div class="container">
-    <a href="#">Homepage</a>
-    <a href="#">Catalog</a>
-    <a href="#">Exercises</a>
-    <a href="#">Contact</a>
-  </div>
-</nav>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Opencode</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+  </head>
+  <body>
+    <header>
+      <div class="sign">
+        <a href="#">Inscription</a>
+        <a href="#">Connexion</a>
+      </div>
+      <nav class="navbar navbar-default navbar-static-top">
+        <div class="container">
+          <a href="#">Homepage</a>
+          <a href="#">Catalog</a>
+          <a href="#">Exercises</a>
+          <a href="#">Contact</a>
+        </div>
+      </nav>
+    </header>
--- index.php
+++ index.php
@@ -1,18 +1,5 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>Opencode</title>
-    <link rel="stylesheet" href="assets/css/style.css">
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-  </head>
-  <body>
-    <header>
+
       <?php
       include 'header.php';
        ?>
-    </header>
-
-
-  </body>
-</html>
+    
