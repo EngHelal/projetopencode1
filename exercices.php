b0b084557fad5fbde61041baa95b7c4b1aab7b80
Victorien HUIBANT <huibant.victorien@gmail.com>
2017-10-04 14:46:12 +0200

    intégration exercice HTML

---
+++ exercices.php
@@ -0,0 +1,36 @@
+<body>
+  <p>Exercice 1 HTML</p>
+  <p>Créez un Tableau avec un &lt;thead&gt;, un &lt;tfoot&gt; et un&lt;tbody&gt;</p>
+  <p>Dans le &lt;thead&gt; le titre du tableau.</p>
+  <p>Dans le &lt;tbody&gt; le contenu du tableau de 3 colonnes et 4 lignes(contenu de votre choix).</p>
+  <p>Dans le &lt;tfoot&gt; Le lien de votre profile github qui cible un nouvel onglet.</p>
+  <p>Pensez à l'indentation !<br/><br/>
+    <div contenteditable="true">
+      &lt;!DOCTYPE html&gt;<br/>
+      &lt;html&gt;<br/>
+      &lt;head&gt;<br/>
+      &lt;title&gt;&lt;/title&gt;<br/>
+      &lt;/head&gt;<br/>
+      &lt;body&gt;<br/>
+      &lt;table&gt;<br/><br/><br/>
+      &lt;/html&gt;<br/>
+    </div><br/>
+    <input type="button" value="Vérifiez vos réponses"/><br/><br/>
+    <p>Exercice 2 HTML</p>
+    <p>Créez un formulaire qui contient  les élements suivants :</p>
+    <p>Champs de saisie de nom, prénom et e-mail.</p>
+    <p>Deux boutons radio pour le choix du sexe.</p>
+    <p>Des cases à cocher pour choisir les domaines d'activités.(Informatique/Gestion/Pédagogie)</p>
+    <p>Un bouton Envoyer et un bouton annuler</p>
+    <p>Pensez à l'indentation !<br/><br/>
+      <div contenteditable="true">
+        &lt;!DOCTYPE html&gt;<br/>
+        &lt;html&gt;<br/>
+        &lt;head&gt;<br/>
+        &lt;title&gt;&lt;/title&gt;<br/>
+        &lt;/head&gt;<br/>
+        &lt;body&gt;<br/><br/><br/>
+        &lt;/html&gt;<br/>
+      </div><br/>
+      <input type="button" value="Vérifiez vos réponses"/><br/><br/>
+    </body>
