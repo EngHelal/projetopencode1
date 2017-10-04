b91697ad6e659fc52fe8dd4ea346323d8591ae18
Amadou camara <atidiane.camara@gmail.com>
2017-10-04 12:01:12 +0200

    ajout de bootstrap et du fichier header

---
+++ header.php
@@ -0,0 +1,13 @@
+
+<div class="sign">
+  <a href="#">Inscription</a>
+  <a href="#">Connexion</a>
+</div>
+<nav class="navbar navbar-default navbar-static-top">
+  <div class="container">
+    <a href="#">Homepage</a>
+    <a href="#">Catalog</a>
+    <a href="#">Exercises</a>
+    <a href="#">Contact</a>
+  </div>
+</nav>
