8f97c0832d6a48b20bc51714e7b1745b5b65d256
Anthony <anthony_bouillon@outlook.fr>
2017-10-05 12:16:23 +0200

    modiff profil sign css

--- profile.php
+++ profile.php
@@ -1,28 +1,7 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>Votre profil</title>
-    <style>
-    body{
-      text-align: center;
-    }
 
-    a{
-      font-size: 15px;
-    }
-
-
-    div{
-      display: inline-block;
-    }
-
-
-    </style>
-  </head>
-  <body>
-    <h1>Vous êtes sur votre profil</h1>
-    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRt3eNCBWfOik6RKLA4D2zi1335pk-lz0bZ5h4VUlnRV-Pq5ruX"/>
+    <img class="img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRt3eNCBWfOik6RKLA4D2zi1335pk-lz0bZ5h4VUlnRV-Pq5ruX"/>
+    <h1 class="titreprofile">Vous êtes sur votre profil</h1><br><br><br>
+      <div class="p">
       <p>Dernière connection le: 07/10/2017</p>
       <p>Votre pseudo: Gégé</p>
       <p>Nom: Leroux</p>
@@ -31,26 +10,27 @@
       <p>Numéro de téléphone: 01-02-03-08-05 <a href="nothing">modifier votre numéro de téléphone</a></p>
       <p>Adresse mail: Gerardleroux@live.fr <a href="nothing">modifier votre adresse mail</a></p>
       <p>Votre mot de passe: ****** <a href="nothing">modifier votre mot de passe</a></p>
+    </div>
+<br><br><br><br><br><br>
 
-    <div class="block1">
+<div class="contener">
+
+  <div>
+    <h2><strong>Vos apprentissages en cours: </strong></h2> <ol><li>PHP pour débutant</li>
+                                                       <li>Javascript pour débutant</li>
+                                                       <li>Utiliser Unity</li></ol></p>
+                                                     </div>
+    <div>
       <h2><strong>Les exercices éffectués: </strong></h2><ol>
                                       <li>HTML/CSS pour débutant</li>
                                       <li>PHP pour débutant (POO)</li>
                                       <li>Javascript pour débutant</li></p>
                                     </div>
 
-    <div class="block2">
+
+    <div>
       <h2><strong>Vos certificats obtenus: </strong></h2> <ol><li>B.A.B.A du HTML/CSS</li>
                                                               <li>Comprendre le web</li>
                                                               <li>Comprendre la blockchaine</li></ol></p>
                                                             </div>
-
-    <div class="block3">
-      <h2><strong>Vos apprentissage en cours: </strong></h2> <ol><li>PHP pour débutant</li>
-                                                         <li>Javascript pour débutant</li>
-                                                         <li>Utiliser Unity</li></ol></p>
-                                                       </div>
-
-
-  </body>
-</html>
+                                                     </div>
