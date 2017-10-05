9c58be8732b9d2528050104fb63c51e26c0cad1c
Victorien HUIBANT <huibant.victorien@gmail.com>
2017-10-05 11:11:18 +0200

    Changement texte anglais en français

--- login.php
+++ login.php
@@ -10,14 +10,14 @@
       <div class="row">
           <div class="col-md-offset-5 col-md-3">
               <div class="form-login">
-              <h4>Welcome back.</h4>
-              <input type="text" id="userName" class="form-control input-sm chat-input" placeholder="username" />
+              <h4>Bienvenue !</h4>
+              <input type="text" id="userName" class="form-control input-sm chat-input" placeholder="Nom d'utilisateur" />
               </br>
-              <input type="text" id="userPassword" class="form-control input-sm chat-input" placeholder="password" />
+              <input type="text" id="userPassword" class="form-control input-sm chat-input" placeholder="Mot de passe" />
               </br>
               <div class="wrapper">
               <span class="group-btn">
-                  <input id="btn" type="button" name="login" value="login" class="btn btn-primary btn-md"><i class="fa fa-sign-in"></i>
+                  <input id="btn" type="button" name="login" value="connexion" class="btn btn-primary btn-md"><i class="fa fa-sign-in"></i>
               </span>
               <script>
                   $("#btn").click(function(){
