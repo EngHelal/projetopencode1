3f3a80b62287eb1dc50125794ba779c314964045
Amadou camara <atidiane.camara@gmail.com>
2017-10-06 08:43:19 +0200

    recupération de la home page

--- catalog.php
+++ catalog.php
@@ -1,103 +1,85 @@
-<<<<<<< HEAD
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="utf-8">
-  <title>Catalogue</title>
-  <link rel="stylesheet" href="assets/css/style.css" />
-  <style>
-  img{
-    float:left;
-    align:center;
-  }
-  h1 {
-    text-align:center;
-  }
-  </style>
-</head>
-<body>
+
+
   <?php
   include 'header.php';
   ?>
-  <h1>Voici les cours</h1>
 <h1>Les cours disponibles actuellement</h1>
 <section>
-  <div id="Content">
-    <div class="content">
-      <img src="assets/img/javascript.png" alt="Javascript" />
-      <p class="center">
-        Javascript
-      </p>
-    </div>
-    <div class="content">
-      <img src="assets/img/html.png" alt="HTML" />
-      <p class="center">
-        <a href="exercices.php">HTML</a>
-      </p>
-    </div>
-    <div class="content">
-      <img src="assets/img/sql.png" alt="SQL" />
-      <p class="center">
-        SQL
-      </p>
-    </div>
-    <div class="content">
-      <img src="assets/img/xul.png" alt="XUL" />
-      <p class="center">
-        XUL
-      </p>
-    </div>
-    <div class="content">
-      <img src="assets/img/php.png" alt="PHP" />
-      <p class="center">
-        <a href="exercicephp.php">PHP</a>
-      </p>
-    </div>
-    <div class="content">
-      <img src="assets/img/css.png" alt="CSS" />
-      <p class="center">
-        CSS
-      </p>
-    </div>
-    <div class="content">
-      <img src="assets/img/xhtml.png" alt="XHTML" />
-      <p class="center">
-        XHTML
-      </p>
-    </div>
-    <div class="content">
-      <img src="assets/img/java.png" alt="Java" />
-      <p class="center">
-        Java
-      </p>
-    </div>
-    <div class="content">
-      <img src="assets/img/c.png" alt="C" />
-      <p class="center">
-        C
-      </p>
-    </div>
-    <div class="content">
-      <img src="assets/img/c++.png" alt="C++" />
-      <p class="center">
-        C++
-      </p>
-    </div>
-    <div class="content">
-      <img src="assets/img/python.png" alt="Python" />
-      <p class="center">
-        Python
-      </p>
-    </div>
-    <div class="content">
-      <img src="assets/img/ruby.png" alt="Ruby" />
-      <p class="center">
-        Ruby
-      </p>
-    </div>
+  <div class="container">
+      <div id="Content">
+        <div class="content">
+          <img src="assets/img/javascript.png" alt="Javascript" />
+          <p class="center">
+            Javascript
+          </p>
+        </div>
+        <div class="content">
+          <img src="assets/img/html.png" alt="HTML" />
+          <p class="center">
+            <a href="exercices.php">HTML</a>
+          </p>
+        </div>
+        <div class="content">
+          <img src="assets/img/sql.png" alt="SQL" />
+          <p class="center">
+            SQL
+          </p>
+        </div>
+        <div class="content">
+          <img src="assets/img/xul.png" alt="XUL" />
+          <p class="center">
+            XUL
+          </p>
+        </div>
+        <div class="content">
+          <img src="assets/img/php.png" alt="PHP" />
+          <p class="center">
+            <a href="exercicephp.php">PHP</a>
+          </p>
+        </div>
+        <div class="content">
+          <img src="assets/img/css.png" alt="CSS" />
+          <p class="center">
+            CSS
+          </p>
+        </div>
+        <div class="content">
+          <img src="assets/img/xhtml.png" alt="XHTML" />
+          <p class="center">
+            XHTML
+          </p>
+        </div>
+        <div class="content">
+          <img src="assets/img/java.png" alt="Java" />
+          <p class="center">
+            Java
+          </p>
+        </div>
+        <div class="content">
+          <img src="assets/img/c.png" alt="C" />
+          <p class="center">
+            C
+          </p>
+        </div>
+        <div class="content">
+          <img src="assets/img/c++.png" alt="C++" />
+          <p class="center">
+            C++
+          </p>
+        </div>
+        <div class="content">
+          <img src="assets/img/python.png" alt="Python" />
+          <p class="center">
+            Python
+          </p>
+        </div>
+        <div class="content">
+          <img src="assets/img/ruby.png" alt="Ruby" />
+          <p class="center">
+            Ruby
+          </p>
+        </div>
+      </div>
+    </section>
   </div>
-</body>
-</html>
-  </div>
-</section>
 <?php include 'footer.php'; ?>
--- homepage.php
+++ homepage.php
@@ -1,15 +1,7 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="assets/css/style.css">
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-    <title>Acceuil</title>
+
     <?php include 'header.php' ?>
-  </head>
-  <body><br><br><br><br>
     <div id="imghomepage">
-    <h1 id="home">Bienvenue sur Opencode !</h1><br>
+    <h1 id="home">Bienvenue sur Opencode !</h1>
     <div>
       <p class="img"><div class="container">
           <div class="row">
@@ -24,30 +16,7 @@
                   <span class="group-btn">
                       <input id="btn" type="button" name="login" value="connexion" class="btn btn-primary btn-md"><i class="fa fa-sign-in"></i>
                   </span>
-                  <script>
-                      $("#btn").click(function(){
-                        var user = $('#userName').val();
-                        var password = $('#userPassword').val();
-                        if (user !== '' && password) !== '' {
-                        }
-                      })
-                  </script>
                   </div>
                   </div>
     </div>
-<br><br><br><br><br><br><br><br><br><br>
-      <p id="text">Ce site propose de nombreuses ressources gratuites pour vous former à l'univers du web.
-      Ainsi, vous pourrez notamment trouver des cours et dossiers complets sur différents langages de programmation comme
-      le HTML, CSS, PHP, C, C++...
-      Les cours sont destinés au débutant.
-      Les cours comportent une difficulté progressive : nous commencerons toujours par créer de bonnes bases avant d'aller plus loin.
-      Attention cependant : il vous faudra de l'implication et de la motivation afin de retirer le maximum de chaque cours.
-      En effet, ne pensez pas que vous pourrez toujours pratiquer immédiatement et tout comprendre d'un coup.
-      Parfois, la théorie va être essentielle et tout ne sera pas facile.
-      Cependant, si vous êtes prêt à relever ce challenge, ce site devrait vous aider à vous former de manière efficace à la programmation
-      informatique.</p><br>
-      <p id="goodluck">Pour commencer et choisir vos exercices veuillez appuyer sur catalogue en haut du site => Bonne chance !</p>
-    </div><br><br><br><br>
-  </body>
-</html>
 <?php include 'footer.php' ?>
