ec6c6d24f096ef46377a8f6193997bb239b4cb6c
Maxime DEFRETIN <maxime.defretin@laposte.net>
2017-10-06 09:42:03 +0200

    login.php modifié

--- login.php
+++ login.php
@@ -1,12 +1,9 @@
 <?php include 'header.php'?>
-
-</head>
-<body>
   <fieldset id="formulairelogin">
     <form method="post" action="quelquepart.php">
       <legend>Formulaire de connexion</legend>
       <p><label for="mail">Entrer votre adresse mail: </label><input type="email" name="email" id="mail"></p>
-      <p><label for="password">Entrer votre mot de passe: <input type="password" name="password" id="password" maxlength="15"></p>
+      <p><label for="password">Entrer votre mot de passe: </labe><input type="password" name="password" id="password" maxlength="15"></p>
       </form>
       <p><input type="submit" value="Connexion" /></p>
       <p><a href="">Mot de passe perdu</a> <a href="">Adresse mail perdu</a></p>
