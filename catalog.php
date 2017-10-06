5cfb3b85d126786fdd275bab94207734b62d0786
Victorien HUIBANT <huibant.victorien@gmail.com>
2017-10-06 08:49:30 +0200

    correction titre et ajout include sur les sexercices

--- catalog.php
+++ catalog.php
@@ -1,9 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="utf-8">
-  <title>Catalogue</title>
-  <link rel="stylesheet" href="assets/css/style.css" />
   <style>
   img{
     float:left;
@@ -19,84 +13,81 @@
   include 'header.php';
   ?>
   <h1>Voici les cours</h1>
-<h1>Les cours disponibles actuellement</h1>
-<section>
-  <div id="Content">
-    <div class="content">
-      <img src="assets/img/javascript.png" alt="Javascript" title="Javascript"/>
-      <p class="center">
-        Javascript
-      </p>
+  <h1>Les cours disponibles actuellement</h1>
+  <section>
+    <div id="Content">
+      <div class="content">
+        <img src="assets/img/javascript.png" alt="Javascript" title="Javascript"/>
+        <p class="center">
+          Javascript
+        </p>
+      </div>
+      <div class="content">
+        <img src="assets/img/html.png" alt="HTML" title="HTML"/>
+        <p class="center">
+          <a href="exercices.php">HTML</a>
+        </p>
+      </div>
+      <div class="content">
+        <img src="assets/img/sql.png" alt="SQL" title="SQL"/>
+        <p class="center">
+          SQL
+        </p>
+      </div>
+      <div class="content">
+        <img src="assets/img/xul.png" alt="XUL" title="XUL"/>
+        <p class="center">
+          XUL
+        </p>
+      </div>
+      <div class="content">
+        <img src="assets/img/php.png" alt="PHP" title="PHP"/>
+        <p class="center">
+          <a href="exercicephp.php">PHP</a>
+        </p>
+      </div>
+      <div class="content">
+        <img src="assets/img/css.png" alt="CSS" title="CSS"/>
+        <p class="center">
+          CSS
+        </p>
+      </div>
+      <div class="content">
+        <img src="assets/img/xhtml.png" alt="XHTML" title="XHTML" />
+        <p class="center">
+          XHTML
+        </p>
+      </div>
+      <div class="content">
+        <img src="assets/img/java.png" alt="Java" title="Java" />
+        <p class="center">
+          Java
+        </p>
+      </div>
+      <div class="content">
+        <img src="assets/img/c.png" alt="C" title="C" />
+        <p class="center">
+          C
+        </p>
+      </div>
+      <div class="content">
+        <img src="assets/img/c++.png" alt="C++" title="C++" />
+        <p class="center">
+          C++
+        </p>
+      </div>
+      <div class="content">
+        <img src="assets/img/python.png" alt="Python" title="Python" />
+        <p class="center">
+          Python
+        </p>
+      </div>
+      <div class="content">
+        <img src="assets/img/ruby.png" alt="Ruby" title="Ruby" />
+        <p class="center">
+          Ruby
+        </p>
+      </div>
     </div>
-    <div class="content">
-      <img src="assets/img/html.png" alt="HTML" title="HTML"/>
-      <p class="center">
-        <a href="exercices.php">HTML</a>
-      </p>
-    </div>
-    <div class="content">
-      <img src="assets/img/sql.png" alt="SQL" title="XUL"/>
-      <p class="center">
-        SQL
-      </p>
-    </div>
-    <div class="content">
-      <img src="assets/img/xul.png" alt="XUL" title="XUL"/>
-      <p class="center">
-        XUL
-      </p>
-    </div>
-    <div class="content">
-      <img src="assets/img/php.png" alt="PHP" title="PHP"/>
-      <p class="center">
-        <a href="exercicephp.php">PHP</a>
-      </p>
-    </div>
-    <div class="content">
-      <img src="assets/img/css.png" alt="CSS" title="CSS"/>
-      <p class="center">
-        CSS
-      </p>
-    </div>
-    <div class="content">
-      <img src="assets/img/xhtml.png" alt="XHTML" title="XHTML" />
-      <p class="center">
-        XHTML
-      </p>
-    </div>
-    <div class="content">
-      <img src="assets/img/java.png" alt="Java" title="Java" />
-      <p class="center">
-        Java
-      </p>
-    </div>
-    <div class="content">
-      <img src="assets/img/c.png" alt="C" title="C" />
-      <p class="center">
-        C
-      </p>
-    </div>
-    <div class="content">
-      <img src="assets/img/c++.png" alt="C++" title="C++" />
-      <p class="center">
-        C++
-      </p>
-    </div>
-    <div class="content">
-      <img src="assets/img/python.png" alt="Python" title="Python" />
-      <p class="center">
-        Python
-      </p>
-    </div>
-    <div class="content">
-      <img src="assets/img/ruby.png" alt="Ruby" title="Ruby" />
-      <p class="center">
-        Ruby
-      </p>
-    </div>
-  </div>
-</body>
-</html>
-  </div>
-</section>
-<?php include 'footer.php'; ?>
+  </section>
+  <?php include 'footer.php'; ?>
--- exercicephp.php
+++ exercicephp.php
@@ -1,45 +1,41 @@
-<body>
-  <head>
-    <link rel="stylesheet" href="assets/css/style.css">
-    <title>Exercices PHP</title>
-  </head>
-  <h1 id="HTML">Exercices PHP</h1>
-  <div id="Exo1HTML">
-    <div id="énoncéhtml1">
-      <p><strong>Exercice 1 PHP</strong></p>
-      <p>Utilisez l'instruction d'affichage echo pour afficher :</p>
-      <p>Une chaîne de caractères.</p>
-      <p>Une variable contenant un entier.</p>
-      <p>Un texte différent selon qu'une condition est vraie ou fausse.</p>
-      <p>Pensez à l'indentation !<br/><br/>
-      </div>
-      <div id="codehtml1">
-        <div contenteditable="true">
-          <span style="color:red"><br/>
-            &lt;?php<br/><br/>
-            ?&gt;<br/>
-          </span>
-        </div>
-      </div><br/>
-      <input type="button" value="Vérifiez vos réponses"/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+<?php include 'header.php'?>
+<h1 id="HTML">Exercices PHP</h1>
+<div id="Exo1HTML">
+  <div id="énoncéhtml1">
+    <p><strong>Exercice 1 PHP</strong></p>
+    <p>Utilisez l'instruction d'affichage echo pour afficher :</p>
+    <p>Une chaîne de caractères.</p>
+    <p>Une variable contenant un entier.</p>
+    <p>Un texte différent selon qu'une condition est vraie ou fausse.</p>
+    <p>Pensez à l'indentation !<br/><br/>
     </div>
-    <div id="Exo2HTML">
-      <div id="énoncéhtml2">
-        <p><strong>Exercice 2 PHP</strong></p>
-        <p>Produire l'affichage des entiers de 1 à 10 à l'aide d'une boucle while.</p>
-        <p>Même chose avec une boucle for.</p>
-        <p>Placez cette boucle dans une fonction et y faire appel.</p>
-        <p>Créez une variable age et l'initialiser avec une valeur.</p>
-        <p>Si l'age est supérieur ou égale à 18, affichez : Vous êtes majeur, Dans le cas contraire, affichez Vous êtes mineur.</p>
+    <div id="codehtml1">
+      <div contenteditable="true">
+        <span style="color:red"><br/>
+          &lt;?php<br/><br/>
+          ?&gt;<br/>
+        </span>
       </div>
-      <div id="codehtml2">
-        <div contenteditable="true">
-          <span style="color:red"><br/>
-            &lt;?php<br/><br/>
-            ?&gt;<br/>
-          </span>
-        </div>
-      </div><br/>
-      <input type="button" value="Vérifiez vos réponses"/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+    </div><br/>
+    <input type="button" value="Vérifiez vos réponses"/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+  </div>
+  <div id="Exo2HTML">
+    <div id="énoncéhtml2">
+      <p><strong>Exercice 2 PHP</strong></p>
+      <p>Produire l'affichage des entiers de 1 à 10 à l'aide d'une boucle while.</p>
+      <p>Même chose avec une boucle for.</p>
+      <p>Placez cette boucle dans une fonction et y faire appel.</p>
+      <p>Créez une variable age et l'initialiser avec une valeur.</p>
+      <p>Si l'age est supérieur ou égale à 18, affichez : Vous êtes majeur, Dans le cas contraire, affichez Vous êtes mineur.</p>
     </div>
-  </body>
+    <div id="codehtml2">
+      <div contenteditable="true">
+        <span style="color:red"><br/>
+          &lt;?php<br/><br/>
+          ?&gt;<br/>
+        </span>
+      </div>
+    </div><br/>
+    <input type="button" value="Vérifiez vos réponses"/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+  </div>
+  <?php include 'footer.php'?>
